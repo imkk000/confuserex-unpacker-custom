0cc5758678fd071526e22e4c229d87016b4fa26a
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-02-03 00:28:06 +0700

    [Added]: initial project from Confuserex-Dynamic-Unpacker

---
+++ confuserex-unpacker-custom/Program.cs
@@ -0,0 +1,220 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfuserexUnpacker
+{
+    class Program
+    {
+        public static ModuleDefMD module;
+        public static Assembly asm;
+
+        public static bool veryVerbose = false;
+        private static string path = null;
+        private static string mode;
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            optionParser(args);
+            Console.WriteLine("Yeah confuserex unpacker so what");
+
+            if (path == null || mode == null)
+            {
+                Console.WriteLine("Check args make sure path and either -d or -s is included (Dynamic or static)"); Console.ReadLine(); return;
+            }
+
+            module = ModuleDefMD.Load(path);
+
+
+
+            if (mode.ToLower() == "static")
+            {
+                staticRoute();
+
+            }
+            else if (mode.ToLower() == "dynamic")
+            {
+                asm = Assembly.LoadFrom(path);
+                dynamicRoute();
+
+            }
+            else
+            {
+                Console.Write("Yeah erm you might be a bit of an idiot follow the instructions");
+                Console.ReadLine();
+                return;
+            }
+
+
+            ModuleWriterOptions writerOptions = new ModuleWriterOptions(module);
+            writerOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+
+            module.Write(path + "Cleaned.exe", writerOptions);
+            Console.ReadLine();
+        }
+        static void staticRoute()
+        {
+
+            antitamper();
+            Protections.ControlFlowRun.cleaner(module);
+            Staticpacker();
+            try
+            {
+                Console.WriteLine("[!] Cleaning Proxy Calls");
+                int amountProxy = Protections.ReferenceProxy.ProxyFixer(module);
+                Console.WriteLine("[!] Amount Of Proxy Calls Fixed: " + amountProxy);
+                Protections.ControlFlowRun.cleaner(module);
+                Console.WriteLine("[!] Decrytping Strings");
+                int strings = Protections.StaticStrings.Run(module);
+                Console.WriteLine("[!] Amount Of Strings Decrypted: " + strings);
+
+            }
+            catch
+            {
+                Console.WriteLine("error happened somewhere apart from tamper and packer im too lazy to implement proper error handling");
+            }
+        }
+        static void dynamicRoute()
+        {
+
+            antitamper();
+            Protections.ControlFlowRun.cleaner(module);
+            packer();
+            try
+            {
+                Console.WriteLine("[!] Cleaning Proxy Calls");
+                int amountProxy = Protections.ReferenceProxy.ProxyFixer(module);
+                Console.WriteLine("[!] Amount Of Proxy Calls Fixed: " + amountProxy);
+                Protections.ControlFlowRun.cleaner(module);
+                Console.WriteLine("[!] Decrytping Strings");
+                int strings = Protections.Constants.constants();
+                Console.WriteLine("[!] Amount Of Strings Decrypted: " + strings);
+
+            }
+            catch
+            {
+                Console.WriteLine("error happened somewhere apart from tamper and packer im too lazy to implement proper error handling");
+            }
+        }
+        static void optionParser(string[] str)
+        {
+            foreach (string arg in str)
+            {
+                switch (arg)
+                {
+
+                    case "-vv":
+                        veryVerbose = true;
+
+                        break;
+                    case "-d":
+                        mode = "dynamic";
+                        break;
+                    case "-s":
+                        mode = "static";
+                        break;
+                    default:
+                        path = arg;
+                        break;
+                }
+            }
+        }
+        static void packer()
+        {
+            try
+            {
+                if (Protections.Packer.IsPacked(module))
+                {
+                    Console.WriteLine("[!] Compressor Detected");
+                    try
+                    {
+                        Protections.Packer.findLocal();
+                        Console.WriteLine("[!] Compressor Removed Successfully");
+                        Console.WriteLine("[!] Now Cleaning The koi Module");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[!] Compressor Failed To Remove");
+                    }
+
+                    antitamper();
+                    module.EntryPoint = module.ResolveToken(Protections.StaticPacker.epToken) as MethodDef;
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("An error in dynamic packer remover happened");
+            }
+
+        }
+        static void Staticpacker()
+        {
+            try
+            {
+                if (Protections.Packer.IsPacked(module))
+                {
+                    Console.WriteLine("[!] Compressor Detected");
+                    try
+                    {
+                        Protections.StaticPacker.Run(module);
+                        Console.WriteLine("[!] Compressor Removed Successfully");
+                        Console.WriteLine("[!] Now Cleaning The koi Module");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[!] Compressor Failed To Remove");
+                    }
+
+                    antitamper();
+                    module.EntryPoint = module.ResolveToken(Protections.StaticPacker.epToken) as MethodDef;
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("An error in static packer remover happened");
+            }
+
+        }
+        static void antitamper()
+        {
+            try
+            {
+                if (Protections.AntiTamper.IsTampered(module) == true)
+                {
+                    Console.WriteLine("[!] Anti Tamper Detected");
+
+                    byte[] rawbytes = null;
+
+                    var htdgfd = (module).MetaData.PEImage.CreateFullStream();
+
+                    rawbytes = htdgfd.ReadBytes((int)htdgfd.Length);
+                    try
+                    {
+                        module = Protections.AntiTamper.UnAntiTamper(module, rawbytes);
+                        Console.WriteLine("[!] Anti Tamper Removed Successfully");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("[!] Anti Tamper Failed To Remove");
+                    }
+
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("An error in anti tamper remover happened");
+            }
+
+        }
+    }
+}
---
+++ confuserex-unpacker-custom/Protections/Constants.cs
@@ -0,0 +1,50 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protections
+{
+    class Constants
+    {
+        public static int constants()
+        {
+            int amount = 0;
+            var manifestModule = ConfuserexUnpacker.Program.asm.ManifestModule;
+            foreach(TypeDef types in ConfuserexUnpacker.Program.module.GetTypes())
+            {
+                foreach(MethodDef methods in types.Methods)
+                {
+                    if (!methods.HasBody) continue;
+                    for(int i = 0; i < methods.Body.Instructions.Count; i++)
+                    {
+                        if(methods.Body.Instructions[i].OpCode == OpCodes.Call && methods.Body.Instructions[i].Operand.ToString().Contains("tring>")&&methods.Body.Instructions[i].Operand is MethodSpec)
+                        {
+                            if (methods.Body.Instructions[i - 1].IsLdcI4())
+                            {
+                                MethodSpec methodSpec = methods.Body.Instructions[i].Operand as MethodSpec;
+
+                                uint param1 = (uint)methods.Body.Instructions[i - 1].GetLdcI4Value();
+                                var value = (string)manifestModule.ResolveMethod(methodSpec.MDToken.ToInt32()).Invoke(null,new object[] {(uint) param1 });
+                                methods.Body.Instructions[i].OpCode = OpCodes.Nop;
+                                methods.Body.Instructions[i - 1].OpCode = OpCodes.Ldstr;
+                                methods.Body.Instructions[i - 1].Operand = value;
+                                amount++;
+                                if (ConfuserexUnpacker.Program.veryVerbose)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Cyan;
+                                    Console.WriteLine(string.Format("Encrypted String Found In Method {0} With Param of {1} the decrypted string is {2}", methods.Name, param1.ToString(), value));
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return amount;
+        }
+    }
+}
